a314ba6684d038b297ee584f327c2aaded6df14d
Reema <reema.t.aljasir@gmail.com>
2026-03-27 14:16:31 +0300

    Initial commit - Lab 4 project

---
+++ apps/bookmodule/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+class BookmoduleConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.bookmodule'  # هذا المسار يجب أن يعكس مكان التطبيق داخل المشروع
